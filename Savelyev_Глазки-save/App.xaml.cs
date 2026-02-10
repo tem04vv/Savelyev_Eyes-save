87e6d158c80eba3c3137d9ed45aee8b87a7b2f06
Artem Savelyev <04012008artem@gmail.com>
2026-02-10 16:06:43 +0500

    Добавьте файлы проекта.

---
+++ Savelyev_Глазки-save/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Savelyev_Глазки_save
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ Savelyev_Глазки-save/Manager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Savelyev_Глазки_save
+{
+    internal class Manager
+    {
+        public static Frame MainFrame { get; set; }
+    }
+}
