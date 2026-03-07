1b05e9f1daf5fab64b2cb5285fae4b0e77a127ed
Artem Savelyev <04012008artem@gmail.com>
2026-03-07 19:00:19 +0500

    17 лаба

--- Savelyev_Глазки-save/AgentsPage.xaml.cs
+++ Savelyev_Глазки-save/AgentsPage.xaml.cs
@@ -23,11 +23,93 @@ namespace Savelyev_Глазки_save
         public AgentsPage()
         {
             InitializeComponent();
-        }
 
+            SortComboBox.SelectedIndex = 0;
+            FiltrationComboBox.SelectedIndex = 0;
+            UpdateAgents();
+        }
+        private void UpdateAgents()
+        {
+            var currentAgent = SavelyevEntities.GetContext().Agent.ToList();
+
+            if (SortComboBox.SelectedIndex == 1)
+            {
+                currentAgent = currentAgent.OrderBy(a => a.Title).ToList();
+            }
+            if (SortComboBox.SelectedIndex == 2)
+            {
+                currentAgent = currentAgent.OrderByDescending(a => a.Title).ToList();
+            }
+            if (SortComboBox.SelectedIndex == 3)
+            {
+                currentAgent = currentAgent.OrderBy(a => a.Discount).ToList();
+            }
+            if (SortComboBox.SelectedIndex == 4)
+            {
+                currentAgent = currentAgent.OrderByDescending(a => a.Discount).ToList();
+            }
+            if (SortComboBox.SelectedIndex == 5)
+            {
+                currentAgent = currentAgent.OrderBy(a => a.Priority).ToList();
+            }
+            if (SortComboBox.SelectedIndex == 6)
+            {
+                currentAgent = currentAgent.OrderByDescending(a => a.Priority).ToList();
+            }
+
+            if (FiltrationComboBox.SelectedIndex == 1)
+            {
+                currentAgent = currentAgent.Where(a => a.AgentType.Title == "ЗАО").ToList();
+            }
+            if (FiltrationComboBox.SelectedIndex == 2)
+            {
+                currentAgent = currentAgent.Where(a => a.AgentType.Title == "МКК").ToList();
+            }
+            if (FiltrationComboBox.SelectedIndex == 3)
+            {
+                currentAgent = currentAgent.Where(a => a.AgentType.Title == "МФО").ToList();
+            }
+            if (FiltrationComboBox.SelectedIndex == 4)
+            {
+                currentAgent = currentAgent.Where(a => a.AgentType.Title == "ОАО").ToList();
+            }
+            if (FiltrationComboBox.SelectedIndex == 5)
+            {
+                currentAgent = currentAgent.Where(a => a.AgentType.Title == "ООО").ToList();
+            }
+            if (FiltrationComboBox.SelectedIndex == 6)
+            {
+                currentAgent = currentAgent.Where(a => a.AgentType.Title == "ПАО").ToList();
+            }
+
+            currentAgent = currentAgent
+                .Where(a => 
+                    a.Title.ToLower().Contains(SearchTextbox.Text.ToLower()) || 
+                    a.Phone.Contains(SearchTextbox.Text) ||
+                    a.Email.Contains(SearchTextbox.Text)
+                    )
+                .ToList();
+
+            AgentsListView.ItemsSource = currentAgent.ToList();
+        }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Manager.MainFrame.Navigate(new AddEditPage());
+        }
+
+        private void SearchTextbox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateAgents();
+        }
+
+        private void SortComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateAgents();
+        }
+
+        private void FiltrationComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateAgents();
         }
     }
 }
